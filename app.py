655f5e23ffe962efa6523f0adc6f80723b794386
Juliet Mwaura <julietmwaura0@gmail.com>
2025-03-03 17:59:12 +0300

    almost

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-import streamlit as st
+
 import pickle
 import pandas as pd
 
 print(pickle.format_version)
 
 df = pd.read_pickle('movie_data.pkl')
-print (df
+
+similarity = pickle.load(open("similarity.pkl",'rb'))
+
+movies_list = df['title'].values
+
+import streamlit as st
+
+st.header("Netflix Recommender System")
+selectvalue=st.selectbox("select movie from dropdown", df_list)
+
+def recommend(movie):
+    index= new_df[new_df['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similar[index])), reverse=True, key=lambda vector:vector[1])
+    for i in distance[0:5]:
+        print (new_df.iloc[i[0]].title)
